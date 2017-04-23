e329ea5505442959b72bf5cb9f9f911c5e743cf2
gabrielmamani <mamanigabriel2014@gmail.com>
2017-04-23 18:10:40 -0400

    Agregar archivos de proyecto.

---
+++ WebRestWT/Controllers/TramasController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebRestWT.Models;
+using WebRestWT.Models.BD;
+
+namespace WebRestWT.Controllers
+{
+    public class TramasController : ApiController
+    {
+
+        WISETRACKEntities db = new WISETRACKEntities();
+
+        public IEnumerable<TramaSerial> GetAllTramas()
+        {
+            TramaSerial[] trama = new TramaSerial[] { };
+            var result = (from t in db.TramaTemp1
+                          select new TramaSerial
+                          {
+                              Asimut = t.Asimut,
+                              direcciones = t.direcciones,
+                              EstadoGPS = t.EstadoGPS,
+                              EstadoMotor = t.EstadoMotor,
+                              EstadoPuerta = t.EstadoPuerta,
+                              FechaGPS = t.FechaGPS,
+                              ID = t.ID,
+                              IDButton = t.IDButton,
+                              IMEI = t.IMEI,
+                              Kilometraje = t.Kilometraje,
+                              Latitud = t.Latitud,
+                              Longitud = t.Longitud,
+                              NIT = t.NIT,
+                              Nombre = t.Nombre,
+                              Nro = t.Nro,
+                              NroPlaca = t.NroPlaca,
+                              Temperatura = t.Temperatura,
+                              tipov = t.tipov,
+                              Velocidad = t.Velocidad,
+                              VoltajeBateria = t.VoltajeBateria
+                          }).ToList();
+
+            trama = result.ToArray();
+
+            return trama;
+        }
+
+
+        [HttpGet]
+        public IHttpActionResult GetTrama(string placa)
+        {
+            //TramaSerial[] trama = new TramaSerial[] { };
+            var result = (from t in db.TramaTemp1
+                          where t.NroPlaca == placa
+                          select new TramaSerial
+                          {
+                              Asimut = t.Asimut,
+                              direcciones = t.direcciones,
+                              EstadoGPS = t.EstadoGPS,
+                              EstadoMotor = t.EstadoMotor,
+                              EstadoPuerta = t.EstadoPuerta,
+                              FechaGPS = t.FechaGPS,
+                              ID = t.ID,
+                              IDButton = t.IDButton,
+                              IMEI = t.IMEI,
+                              Kilometraje = t.Kilometraje,
+                              Latitud = t.Latitud,
+                              Longitud = t.Longitud,
+                              NIT = t.NIT,
+                              Nombre = t.Nombre,
+                              Nro = t.Nro,
+                              NroPlaca = t.NroPlaca,
+                              Temperatura = t.Temperatura,
+                              tipov = t.tipov,
+                              Velocidad = t.Velocidad,
+                              VoltajeBateria = t.VoltajeBateria
+                          }).FirstOrDefault();
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+    }
+}
+
---
+++ WebRestWT/Models/TramaSerial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRestWT.Models
+{
+    public class TramaSerial
+    {
+        public int Nro { get; set; }
+        public long ID { get; set; }
+        public Nullable<int> EstadoGPS { get; set; }
+        public Nullable<int> EstadoMotor { get; set; }
+        public Nullable<double> Velocidad { get; set; }
+        public Nullable<double> Latitud { get; set; }
+        public Nullable<double> Longitud { get; set; }
+        public Nullable<System.DateTime> FechaGPS { get; set; }
+        public string NroPlaca { get; set; }
+        public string Nombre { get; set; }
+        public Nullable<double> Asimut { get; set; }
+        public string NIT { get; set; }
+        public string IDButton { get; set; }
+        public Nullable<double> Kilometraje { get; set; }
+        public Nullable<double> Temperatura { get; set; }
+        public Nullable<double> VoltajeBateria { get; set; }
+        public Nullable<bool> EstadoPuerta { get; set; }
+        public Nullable<int> tipov { get; set; }
+        public string IMEI { get; set; }
+        public string direcciones { get; set; }
+    }
+}
